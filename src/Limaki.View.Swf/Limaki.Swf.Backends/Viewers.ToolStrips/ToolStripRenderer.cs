ba99add7fcf1aa36a13e27cb1e025d58bfc8b757
lytico <lytico@users.sourceforge.net>
2013-04-25 22:45:20 +0200

    custom ToolStripRenderer

---
+++ src/Limaki.View.Swf/Limaki.Swf.Backends/Viewers.ToolStrips/ToolStripRenderer.cs
@@ -0,0 +1,41 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Limaki.Swf.Backends.Viewers.ToolStrips {
+
+    public class ToolStripRenderer : ToolStripProfessionalRenderer {
+
+        public System.Drawing.Color? ToolStripButtonCheckedColor { get; set; }
+
+        protected Brush _buttonBackgroundBrush = null;
+        protected Brush ButtonBackgroundBrush { get { return _buttonBackgroundBrush ?? (_buttonBackgroundBrush = new SolidBrush(ToolStripButtonCheckedColor.Value)); } }
+
+        protected override void OnRenderButtonBackground(ToolStripItemRenderEventArgs e) {
+            var btn = e.Item as ToolStripButton;
+            if (btn != null && ToolStripButtonCheckedColor.HasValue && (btn.Checked || btn.Selected)) {
+                var bounds = new Rectangle(Point.Empty, e.Item.Size);
+                e.Graphics.FillRectangle(ButtonBackgroundBrush, bounds);
+            } else
+                base.OnRenderButtonBackground(e);
+        }
+
+        protected override void OnRenderItemBackground(ToolStripItemRenderEventArgs e) {
+            var drawBackground = ToolStripButtonCheckedColor.HasValue && e.Item.Selected;
+            if (drawBackground) {
+                var bounds = new Rectangle(Point.Empty, e.Item.Size);
+                e.Graphics.FillRectangle(ButtonBackgroundBrush, bounds);
+            } else
+                base.OnRenderItemBackground(e);
+        }
+
+        protected override void OnRenderDropDownButtonBackground (ToolStripItemRenderEventArgs e) {
+            var drawBackground = ToolStripButtonCheckedColor.HasValue && e.Item.Selected;
+            if (drawBackground) {
+                var bounds = new Rectangle(Point.Empty, e.Item.Size);
+                e.Graphics.FillRectangle(ButtonBackgroundBrush, bounds);
+            } else
+            base.OnRenderDropDownButtonBackground(e);
+        }
+    }
+
+}
