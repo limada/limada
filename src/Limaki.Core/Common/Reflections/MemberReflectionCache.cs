ea6e0f24b365330dad34df435b2b4e1030b87212
lytico <lytico@users.sourceforge.net>
2013-12-06 02:48:05 +0100

    reflections improvements

--- src/Limaki.Core/Common/Reflections/MemberReflectionCache.cs
+++ src/Limaki.Core/Common/Reflections/MemberReflectionCache.cs
@@ -24,22 +24,22 @@ namespace Limaki.Common.Reflections {
         IDictionary<Type, PropertyInfo[]> _members = new Dictionary<Type, PropertyInfo[]>();
         HashSet<Type> _done = new HashSet<Type>();
 
-       
+
         HashSet<int> _membersFiltered = new HashSet<int>();
         IDictionary<int, PropertyInfo> _propertyInfos = new Dictionary<int, PropertyInfo>();
         IDictionary<int, Delegate> _propertySetter = new Dictionary<int, Delegate>();
-        IDictionary<int, MethodInfo> _propertySetMethod = new Dictionary<int, MethodInfo>(); 
+        IDictionary<int, MethodInfo> _propertySetMethod = new Dictionary<int, MethodInfo>();
         IDictionary<int, MethodInfo> _propertyGetMethod = new Dictionary<int, MethodInfo>();
 
-        public MemberReflectionCache() {
+        public MemberReflectionCache () {
             this.BindingFlags = BindingFlags.Public | BindingFlags.Instance;
         }
 
-        public MemberReflectionCache(BindingFlags bindingFlags) {
+        public MemberReflectionCache (BindingFlags bindingFlags) {
             this.BindingFlags = bindingFlags;
         }
 
-        protected int GetHashCode(Type item1, Type item2, string item3) {
+        protected int GetHashCode (Type item1, Type item2, string item3) {
             int h = item1.GetHashCode();
             h = (h << 5) - h + item2.GetHashCode();
             h = (h << 5) - h + item3.GetHashCode();
@@ -47,7 +47,7 @@ namespace Limaki.Common.Reflections {
         }
 
         public BindingFlags BindingFlags { get; protected set; }
-        public void AddType(Type type, Func<PropertyInfo, bool> memberFilter) {
+        public void AddType (Type type, Func<PropertyInfo, bool> memberFilter) {
             if (!_done.Contains(type)) {
 
                 //TODO: look at this:http://www.codeproject.com/KB/cs/HyperPropertyDescriptor.aspx
@@ -66,10 +66,10 @@ namespace Limaki.Common.Reflections {
                 }
             }
         }
-        public void AddType(Type type) {
+        public void AddType (Type type) {
             AddType(type, p => p != null);
         }
-        public PropertyInfo[] Members(Type type, Func<PropertyInfo, bool> memberFilter) {
+        public PropertyInfo[] Members (Type type, Func<PropertyInfo, bool> memberFilter) {
             PropertyInfo[] members = null;
             if (!_members.TryGetValue(type, out members)) {
                 members = type.GetProperties(BindingFlags).Where(memberFilter).ToArray();
@@ -78,20 +78,20 @@ namespace Limaki.Common.Reflections {
             return members;
         }
 
-        public bool ValidMember(Type type, Type propertyType, string memberName) {
+        public bool ValidMember (Type type, Type propertyType, string memberName) {
             var key = GetHashCode(type, propertyType, memberName);
             return _membersFiltered.Contains(key);
         }
 
-        public void SetValue(Type type, Type propertyType, string memberName, object obj, object value) {
+        public void SetValue (Type type, Type propertyType, string memberName, object obj, object value) {
             var key = GetHashCode(type, propertyType, memberName);
 
             var meth = _propertySetMethod[key];
-            meth.Invoke(obj, new object[] {value});
+            meth.Invoke(obj, new object[] { value });
 
         }
 
-        public object GetValue(Type type, Type propertyType, string memberName, object obj) {
+        public object GetValue (Type type, Type propertyType, string memberName, object obj) {
             var key = GetHashCode(type, propertyType, memberName);
 
             var meth = _propertyGetMethod[key];
@@ -99,11 +99,11 @@ namespace Limaki.Common.Reflections {
 
         }
 
-        public PropertyInfo GetPropertyInfo(Type type, Type propertyType, string memberName) {
+        public PropertyInfo GetPropertyInfo (Type type, Type propertyType, string memberName) {
             var key = GetHashCode(type, propertyType, memberName);
             return _propertyInfos[key];
         }
 
-       
+
     }
 }
---
+++ src/Limaki.Core/Common/Reflections/Reflector.cs
@@ -0,0 +1,106 @@
+/*
+ * Limaki 
+ * 
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ * 
+ * Author: Lytico
+ * Copyright (C) 2006-2011 Lytico
+ *
+ * http://www.limada.org
+ * 
+ */
+
+using System;
+using Limaki.Common.Collections;
+using System.Reflection;
+
+namespace Limaki.Common {
+
+    public static class Reflector {
+
+        public static bool IsStorable (Type type) {
+            return !(type.IsPrimitive || type == typeof(string));
+        }
+
+        public static Set<Tuple<Type, Type>> _implements = new Set<Tuple<Type, Type>>();
+        public static bool Implements (Type clazz, Type interfaze) {
+            if (clazz.IsClass) {
+                var key = Tuple.Create(clazz, interfaze);
+                if (_implements.Contains(key))
+                    return true;
+                bool result = (interfaze.IsAssignableFrom(clazz));
+                if (!result && interfaze.IsInterface) {
+                    foreach (Type t in clazz.GetInterfaces()) {
+                        if (t == interfaze) {
+                            result = true;
+                            _implements.Add(key);
+                            break;
+                        }
+                    }
+                }
+                return result;
+            } else {
+                return false;
+            }
+        }
+
+        public static string ClassName (Type type) {
+            var result = type.Name;
+            if (type.IsNested && !type.IsGenericParameter) {
+                result = type.FullName.Replace(type.DeclaringType.FullName + "+", ClassName(type.DeclaringType));
+            }
+            if (type.IsGenericType) {
+
+                var genPos = result.IndexOf('`');
+                if (genPos > 0)
+                    result = result.Substring(0, genPos);
+                else
+                    result = result + "";
+                bool isNullable = result == "Nullable";
+                if (!isNullable)
+                    result += "<";
+                else
+                    result = "";
+                foreach (var item in type.GetGenericArguments())
+                    result += ClassName(item) + ",";
+                result = result.Remove(result.Length - 1, 1);
+                if (isNullable)
+                    result += "?";
+                else
+                    result += ">";
+            }
+            return result;
+        }
+
+        private static A GetSingleAttribute<A>(object[] attributes)
+           where A : Attribute {
+            if (attributes.Length > 0)
+                return (A)attributes[0];
+            return null;
+        }
+
+        /// <summary>
+        /// Returns a requested attribute for a given type
+        /// </summary>
+        /// <typeparam name="A">The requested attribute type</typeparam>
+        /// <param name="t">The class supposed to provide that attribute</param>
+        /// <returns>An attribute of type A or null if none</returns>
+        public static A GetAttribute<A>(this Type t)
+            where A : Attribute {
+            return GetSingleAttribute<A>(t.GetCustomAttributes(typeof(A), true));
+        }
+
+        /// <summary>
+        /// Returns a requested attribute for a given member
+        /// </summary>
+        /// <typeparam name="A">The requested attribute type</typeparam>
+        /// <param name="m">The member supposed to provide that attribute</param>
+        /// <returns>An attribute of type A or null if none</returns>
+        public static A GetAttribute<A>(this MemberInfo m)
+            where A : Attribute {
+            return GetSingleAttribute<A>(m.GetCustomAttributes(typeof(A), true));
+        }
+    }
+}
