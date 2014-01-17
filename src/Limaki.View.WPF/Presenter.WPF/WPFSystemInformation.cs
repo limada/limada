639a4c7a9f494f6ea39eb84136e47874f23803eb
lytico <lytico@users.sourceforge.net>
2014-01-17 04:09:31 +0100

    Wpf: some SystemInformations

--- src/Limaki.View.WPF/Presenter.WPF/WPFSystemInformation.cs
+++ src/Limaki.View.WPF/Presenter.WPF/WPFSystemInformation.cs
@@ -3,18 +3,22 @@ using System.Windows;
 using Limaki.View.UI;
 using Xwt;
 using Size = Xwt.Size;
+using System.Runtime.InteropServices;
 
 
 namespace Limaki.View.WPF {
     public class WPFSystemInformation : IUISystemInformation {
         public Size DragSize {
             get {
-                return new Size(12,12);
+                return new Size(SystemParameters.MinimumHorizontalDragDistance, SystemParameters.MinimumHorizontalDragDistance);
             }
         }
+
+        [DllImport("user32.dll")]
+        static extern uint GetDoubleClickTime ();
         public int DoubleClickTime {
             get {
-                return 12;
+                return (int)GetDoubleClickTime(); 
             }
         }
         public int VerticalScrollBarWidth {
