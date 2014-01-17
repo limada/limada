993f84ba29b8c2e57115297dcab707f9d55f2a54
lytico <lytico@users.sourceforge.net>
2014-01-17 20:08:43 +0100

    ImageDisplay based on Xwt.Drawing.Image (no backend yet)

---
+++ src/Limaki.View/Limaki.View/Visualizers/ImageDisplay.cs
@@ -0,0 +1,57 @@
+using Limaki.View.UI;
+using Limaki.View.Visualizers;
+using Xwt;
+using Xwt.Backends;
+using Xwt.Drawing;
+
+namespace Limaki.View.XwtBackend {
+    /// <summary>
+    /// a display based on Xwt.Drawing.Image
+    /// </summary>
+    
+    [BackendType(typeof(IImageDisplayBackend))]
+    public class ImageDisplay : Display<Image> { }
+    
+
+    public interface IImageDisplayBackend : IDisplayBackend<Image> { }
+
+    public class ImageDisplayFactory : DisplayFactory<Image> {
+        public override Display<Image> Create () {
+            return new ImageDisplay();
+        }
+    }
+
+    public class ImageDisplayComposer : DisplayComposer<Image> {
+
+        public override void Compose (Display<Image> display) {
+
+            this.DataOrigin = () => { return Point.Zero; };
+            this.DataSize = () => {
+                var data = display.Data;
+                if (data != null)
+                    return data.Size;
+                else
+                    return Size.Zero;
+            };
+
+            base.Compose(display);
+
+            Compose(display, display.SelectionRenderer);
+            Compose(display, display.MoveResizeRenderer);
+
+            var selectAction = new SelectorAction();
+            Compose(display, selectAction, true);
+            selectAction.ShowGrips = false;
+            selectAction.Enabled = false;
+
+            display.SelectAction = selectAction;
+            display.EventControler.Add(selectAction);
+
+            display.SelectAction.ShowGrips = true;
+            display.SelectAction.Enabled = false;
+            display.MouseScrollAction.Enabled = true;
+
+        }
+    }
+
+}
