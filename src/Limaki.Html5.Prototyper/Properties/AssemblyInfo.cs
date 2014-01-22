516148cc39a33e4da423fa6b91c00c0bc3cafd45
lytico <lytico@users.sourceforge.net>
2014-01-22 22:35:09 +0100

    fix some assemblyInfos

--- src/Limaki.Html5.Prototyper/Properties/AssemblyInfo.cs
+++ src/Limaki.Html5.Prototyper/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Xwt.Html5.Prototyper")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright Limada.org © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
