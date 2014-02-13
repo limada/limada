92f7bb0599f50cd285d244c50e367896cc37cf83
lytico <lytico@users.sourceforge.net>
2014-02-13 16:40:30 +0100

    WiredScenes: extract SceneChanger baseclass

---
+++ src/Limaki.View/Limaki.View/Visuals/UI/SceneChanger.cs
@@ -0,0 +1,136 @@
+using System.Collections.Generic;
+using Limaki.Drawing;
+using Limaki.Graphs;
+using Limaki.View.Layout;
+using Limaki.View.UI.GraphScene;
+using Limaki.Visuals;
+using Xwt;
+
+namespace Limaki.View.Visuals.UI {
+
+    public class SceneChanger {
+
+        public SceneChanger (IGraphScene<IVisual, IVisualEdge> target) {
+            this.Target = target;
+        }
+
+        public IGraphScene<IVisual, IVisualEdge> Target { get; protected set; }
+
+
+        public virtual IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge> TargetGraph {
+            get { return Target.Graph as IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge>; }
+        }
+
+        public virtual void DataChanged (IVisual item) {
+            Target.Requests.Add(new LayoutCommand<IVisual>(item, LayoutActionType.Perform));
+        }
+
+        protected virtual void Remove (IVisual item) {
+
+            if (Target.Contains(item)) {
+                if (Target.Focused == item) {
+                    Target.Focused = null;
+                }
+                Target.Selected.Remove(item);
+
+                Target.Requests.Add(new RemoveBoundsCommand<IVisual, IVisualEdge>(item, Target));
+                TargetGraph.Remove(item);
+            } else {
+                if (TargetGraph.Source.Contains(item)) {
+                    //var sinkGraph = TargetGraph.Source as ISinkGraph<IVisual, IVisualEdge>;
+                    //if (sinkGraph != null)
+                    //    sinkGraph.RemoveInSink(item);
+                    //else
+                        TargetGraph.Source.Remove(item);
+                }
+            }
+        }
+
+        protected virtual void Add (IVisualEdge edge) {
+            if (Target.Contains(edge.Root) && (Target.Contains(edge.Leaf))) {
+                Target.Graph.Add(edge);
+                if (edge.Shape == null) {
+                    Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Invoke));
+                }
+                Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Justify));
+            }
+        }
+
+        protected virtual void ChangeEdge (IVisual root, IVisual leaf, IVisualEdge targetEdge) {
+
+            if (targetEdge.Root != root || targetEdge.Leaf != leaf) {
+
+                bool isEdgeVisible = TargetGraph.Sink.Contains(targetEdge);
+                bool makeEdgeVisible = TargetGraph.Sink.Contains(root) && TargetGraph.Sink.Contains(leaf);
+
+                if (targetEdge.Root != root) {
+                    if (makeEdgeVisible)
+                        TargetGraph.ChangeEdge(targetEdge, root, true);
+                    else
+                        TargetGraph.Source.ChangeEdge(targetEdge, root, true);
+                }
+
+                if (targetEdge.Leaf != leaf) {
+                    if (makeEdgeVisible)
+                        TargetGraph.ChangeEdge(targetEdge, leaf, false);
+                    else
+                        TargetGraph.Source.ChangeEdge(targetEdge, leaf, false);
+                }
+
+
+                if (makeEdgeVisible) {
+                    List<IVisualEdge> changeList = new List<IVisualEdge>();
+                    changeList.Add(targetEdge);
+                    changeList.AddRange(TargetGraph.Source.Twig(targetEdge));
+
+                    foreach (IVisualEdge edge in changeList) {
+                        bool showTwig = (Target.Contains(edge.Root) && Target.Contains(edge.Leaf));
+
+                        bool doAdd = (edge == targetEdge && !isEdgeVisible) ||
+                                     (!TargetGraph.Sink.Contains(edge) && showTwig);
+
+                        if (doAdd) {
+                            TargetGraph.Sink.Add(edge);
+                            if (edge.Shape == null) {
+                                Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Invoke));
+                            } else {
+                                edge.Size = Size.Zero;
+                            }
+                        }
+                        if (showTwig || edge == targetEdge)
+                            Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Justify));
+                    }
+                } else {
+                    ICollection<IVisualEdge> changeList = new List<IVisualEdge>(TargetGraph.Sink.Twig(targetEdge));
+                    changeList.Add(targetEdge);
+                    foreach (IVisualEdge edge in changeList) {
+                        TargetGraph.Sink.Remove(edge);
+                        Target.Requests.Add(new RemoveBoundsCommand<IVisual, IVisualEdge>(edge, Target));
+                    }
+                }
+                //foreach (var edge in target.Twig(targetEdge)) {
+                //    if (makeEdgeVisible) {
+                //        target.Commands.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Justify));
+                //    } else {
+                //        targetGraph.Sink.Remove(edge);
+                //        target.Commands.Add(new RemoveBoundsCommand(edge, target));
+                //    }
+                //}
+            }
+        }
+
+        public virtual void GraphChanged (IVisual item, GraphChangeType changeType) {
+            if (item != null) {
+                if (changeType == GraphChangeType.Remove) {
+                    Remove(item);
+                    return;
+                }
+                if (changeType == GraphChangeType.Add && item is IVisualEdge) {
+                    Add((IVisualEdge) item);
+                    return;
+                }
+
+            }
+        }
+    }
+}
--- src/Limaki.View/Limaki.View/Visuals/UI/WiredScenes.cs
+++ src/Limaki.View/Limaki.View/Visuals/UI/WiredScenes.cs
@@ -27,154 +27,51 @@ namespace Limaki.View.Visuals.UI {
     /// encaspulates some operations on Scenes wich are linked to each other
     /// used to build Graph.DataChanged and Graph.GraphChanged events
     /// </summary>
-    public class WiredScenes {
-        public WiredScenes(IGraphScene<IVisual, IVisualEdge> source, IGraphScene<IVisual, IVisualEdge> target) {
-            this.Source = source;
-            this.Target = target;
+    public class WiredScenes : SceneChanger {
+        public WiredScenes (IGraph<IVisual, IVisualEdge> sourceGraph, IGraphScene<IVisual, IVisualEdge> target)
+            : base(target) {
+            this.SourceGraph = sourceGraph as IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge>;
         }
 
-        
-        public IGraphScene<IVisual, IVisualEdge> Source { get; protected set;}
-        public IGraphScene<IVisual, IVisualEdge> Target { get; protected set; }
+        public WiredScenes (IGraphScene<IVisual, IVisualEdge> source, IGraphScene<IVisual, IVisualEdge> target)
+            : base(target) {
 
-        public virtual IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge> SourceGraph {
-            get { return Source.Graph as IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge>; }
+            this.SourceGraph = source.Graph as IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge>;
         }
 
-        public virtual IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge> TargetGraph {
-            get { return Target.Graph as IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge>; }
-        }
 
-        public virtual IVisual LookUp(IVisual sourceitem) {
+        public virtual IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge> SourceGraph { get; protected set; }
+
+
+        public virtual IVisual LookUp (IVisual sourceitem) {
             return GraphMapping.Mapping.LookUp<IVisual, IVisualEdge>(SourceGraph, TargetGraph, sourceitem);
         }
 
-        public virtual void DataChanged(IVisual sourceItem) {
-            IVisual item = LookUp(sourceItem);
+        public override void DataChanged (IVisual sourceItem) {
+            var item = LookUp(sourceItem);
             if (item != null && !item.Data.Equals(sourceItem.Data)) {
                 item.Data = sourceItem.Data;
-                Target.Requests.Add(new LayoutCommand<IVisual>(item, LayoutActionType.Perform));
+                base.DataChanged(item);
             }
         }
 
-        protected virtual void Remove(IVisual item) {
-            ICollection<IVisualEdge> deleteQueue = 
-                new List<IVisualEdge>(TargetGraph.Source.PostorderTwig(item));
-
-            foreach (var delete in deleteQueue) {
-                if (Target.Contains (delete)) {
-                    // remark: it is not allowed to use DeleteCommand here as it calls
-                    // OnDataChanged and 
-                    Target.Requests.Add(new RemoveBoundsCommand<IVisual,IVisualEdge>(delete, Target));
-                    Target.Remove (delete);
-                } else {
-                    TargetGraph.Source.Remove (delete);
-                }
-            }
-
-            if (Target.Contains(item)) {
-                if (Target.Focused == item) {
-                    Target.Focused = null;
-                }
-                Target.Selected.Remove(item);
-
-                Target.Requests.Add(new RemoveBoundsCommand<IVisual, IVisualEdge>(item, Target));
-                TargetGraph.Remove(item);
-            } else {
-                TargetGraph.Source.Remove (item);
-            }
+        protected virtual void ChangeEdge (IVisualEdge sourceEdge, IVisualEdge targetEdge) {
+            var root = LookUp(sourceEdge.Root);
+            var leaf = LookUp(sourceEdge.Leaf);
+            base.ChangeEdge(root, leaf, targetEdge);
         }
 
-        protected virtual void Add(IVisualEdge edge) {
-            if (Target.Contains(edge.Root) && (Target.Contains(edge.Leaf))) {
-                Target.Graph.Add(edge);
-                if (edge.Shape == null) {
-                    Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Invoke));
-                }
-                Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Justify));
-            }
-        }
-
-        protected virtual void ChangeEdge(IVisualEdge sourceEdge, IVisualEdge targetEdge) {
-            IVisual root = LookUp(sourceEdge.Root);
-            IVisual leaf = LookUp(sourceEdge.Leaf);
-            if (targetEdge.Root != root || targetEdge.Leaf != leaf) {
-                
-                bool isEdgeVisible = TargetGraph.Sink.Contains(targetEdge);
-                bool makeEdgeVisible = TargetGraph.Sink.Contains(root) && TargetGraph.Sink.Contains(leaf);
-
-                if (targetEdge.Root != root) {
-                    if (makeEdgeVisible)
-                        TargetGraph.ChangeEdge(targetEdge, root, true);
-                    else
-                        TargetGraph.Source.ChangeEdge(targetEdge, root, true);
-                }
-
-                if (targetEdge.Leaf != leaf) {
-                    if (makeEdgeVisible)
-                        TargetGraph.ChangeEdge(targetEdge, leaf, false);
-                    else
-                        TargetGraph.Source.ChangeEdge(targetEdge, leaf, false);
-                }
-
-
-                if (makeEdgeVisible) {
-                    List<IVisualEdge> changeList = new List<IVisualEdge>();
-                    changeList.Add(targetEdge);
-                    changeList.AddRange(TargetGraph.Source.Twig(targetEdge));
-
-                    foreach (IVisualEdge edge in changeList) {
-                        bool showTwig = (Target.Contains(edge.Root) && Target.Contains(edge.Leaf));
-
-                        bool doAdd = ( edge == targetEdge && !isEdgeVisible ) ||
-                                     ( !TargetGraph.Sink.Contains (edge) && showTwig );
-
-                        if (doAdd) {
-                            TargetGraph.Sink.Add(edge);
-                            if (edge.Shape == null) {
-                                Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Invoke));
-                            } else {
-                                edge.Size = Size.Zero;
-                            }
-                        }
-                        if (showTwig || edge==targetEdge)
-                            Target.Requests.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Justify));
-                    }
-                } else {
-                    ICollection<IVisualEdge> changeList = new List<IVisualEdge>(TargetGraph.Sink.Twig(targetEdge));
-                    changeList.Add(targetEdge);
-                    foreach (IVisualEdge edge in changeList) {
-                        TargetGraph.Sink.Remove(edge);
-                        Target.Requests.Add(new RemoveBoundsCommand<IVisual, IVisualEdge>(edge, Target));
-                    }
-                }
-                //foreach (var edge in target.Twig(targetEdge)) {
-                //    if (makeEdgeVisible) {
-                //        target.Commands.Add(new LayoutCommand<IVisual>(edge, LayoutActionType.Justify));
-                //    } else {
-                //        targetGraph.Sink.Remove(edge);
-                //        target.Commands.Add(new RemoveBoundsCommand(edge, target));
-                //    }
-                //}
-            }
-        }
-
-        public virtual void GraphChanged(IVisual sourceItem, GraphChangeType changeType) {
-            IVisual item = LookUp(sourceItem);
+        public override void GraphChanged (IVisual sourceItem, GraphChangeType changeType) {
+            var item = LookUp(sourceItem);
             if (item != null) {
-                if (changeType == GraphChangeType.Remove) {
-                    Remove(item);
-                    return;
-                }
-                if (changeType == GraphChangeType.Add && item is IVisualEdge) {
-                    Add((IVisualEdge)item);
-                    return;
-                }
                 if (changeType == GraphChangeType.Update && item is IVisualEdge) {
-                    ChangeEdge((IVisualEdge)sourceItem, (IVisualEdge)item);
+                    ChangeEdge((IVisualEdge) sourceItem, (IVisualEdge) item);
                     return;
+                } else {
+                    base.GraphChanged(item, changeType);
                 }
             }
+
         }
     }
 }
