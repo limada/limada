a56f62f3ec9adeef56480464f09ad853f7f47dd6
lytico <lytico@users.sourceforge.net>
2014-02-19 05:22:22 +0100

    SceneFacadeTest: introduced a Wrapper for easier handling | GraphSceneMeshTest: refactored to use TestWraper and  tests extended to multiple Displays

---
+++ src/Limaki.Tests/Limaki.Tests/View/Visuals/Folding/SceneFacadeTestWrapper.cs
@@ -0,0 +1,190 @@
+using System.Collections.Generic;
+using Limaki.Drawing;
+using Limaki.Graphs;
+using Limaki.Graphs.Extensions;
+using Limaki.Model;
+using Limaki.Tests.Graph.Model;
+using Limaki.Tests.Graph.Wrappers;
+using Limaki.View.Layout;
+using Limaki.View.UI.GraphScene;
+using Limaki.View.Visualizers;
+using Limaki.Visuals;
+using NUnit.Framework;
+
+namespace Limaki.Tests.View.Visuals {
+
+    public class SceneFacadeTestWrapper<TFactory> 
+        where TFactory : TestGraphFactory<IGraphEntity, IGraphEdge>, new () {
+
+        /// <summary>
+        /// sets Scene.Focus to item and 
+        /// Expands(deep) it
+        /// item is added if not in view
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="deep"></param>
+        public void Expand (IVisual item, bool deep) {
+            Scene.Selected.Clear ();
+            SetFocused (item);
+            SceneFacade.Expand (deep);
+            Test.CommandsPerform ();
+        }
+
+        public IVisual ChangeLink (IVisualEdge edge, IVisual item, bool root) {
+
+            var newItem = item;
+            var oldItem = root ? edge.Root : edge.Leaf;
+
+            Scene.ChangeEdge (edge, newItem, root);
+            Scene.Graph.OnGraphChanged (edge, GraphEventType.Update);
+
+            Scene.Requests.Add (new LayoutCommand<IVisual> (edge, LayoutActionType.Justify));
+            foreach (var twig in Scene.Twig (edge)) {
+                Scene.Requests.Add (new LayoutCommand<IVisual> (twig, LayoutActionType.Justify));
+            }
+
+            Test.CommandsPerform ();
+
+            return oldItem;
+        }
+
+        public void ProoveChangedLink (IVisualEdge edge, IVisual newItem, IVisual oldItem, bool root) {
+            ProoveChangedLink (edge, newItem, oldItem, root, true);
+        }
+
+        public void ProoveChangedLink (IVisualEdge edge, IVisual newItem, IVisual oldItem, bool root, bool inView) {
+            
+            Assert.IsNotNull (edge);
+            Assert.AreSame (root ? edge.Root : edge.Leaf, newItem);
+            Assert.AreNotSame (root ? edge.Root : edge.Leaf, oldItem);
+
+            if (inView) {
+                Assert.IsTrue (View.Edges (newItem).Contains (edge));
+                Assert.IsFalse (View.Edges (oldItem).Contains (edge));
+            } else {
+                Assert.IsTrue (Graph.Edges (newItem).Contains (edge));
+                Assert.IsFalse (Graph.Edges (oldItem).Contains (edge));
+            }
+        }
+
+        /// <summary>
+        /// sets Scene.Focus to item and 
+        /// calls Layout.Perform and Layout.AdjustSize
+        /// item is added if not in view
+        /// </summary>
+        public void SetFocused (IVisual item) {
+            Mock.SetFocused (item);
+        }
+
+        public void EnsureShape (IVisual item) {
+            Mock.EnsureShape (item);
+        }
+
+        /// <summary>
+        /// make a new link
+        /// add it to Scene
+        /// call View.OnGraphChanged 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="leaf"></param>
+        /// <returns></returns>
+        public IVisualEdge AddEdge (IVisual root, IVisual leaf) {
+
+            var sourceEdge = new VisualEdge<string> ("", root, leaf);
+            sourceEdge.Data = GraphExtensions.EdgeString<IVisual, IVisualEdge> (sourceEdge);
+
+            Scene.Add (sourceEdge);
+            Scene.Graph.OnGraphChanged (sourceEdge, GraphEventType.Add);
+            return sourceEdge;
+        }
+
+        public void RemoveEdge (IVisualEdge edge) {
+            this.Scene.Graph.OnGraphChanged (edge, GraphEventType.Remove);
+            this.Scene.Remove (edge);
+        }
+
+        public void ProveContains (IGraph<IVisual, IVisualEdge> graph, params IVisual[] visuals) {
+            foreach (var item in visuals)
+                if (item is IVisualEdge)
+                    Assert.IsTrue (graph.Contains ((IVisualEdge) item));
+                else
+                    Assert.IsTrue (graph.Contains (item));
+        }
+
+        public void ProveNotContains (IGraph<IVisual, IVisualEdge> graph, params IVisual[] visuals) {
+            foreach (var item in visuals)
+                if (item is IVisualEdge)
+                    Assert.IsFalse (graph.Contains ((IVisualEdge) item));
+                else
+                    Assert.IsFalse (graph.Contains (item));
+        }
+
+        /// <summary>
+        /// tests if View.Sink contains visuals
+        /// </summary>
+        /// <param name="visuals"></param>
+        public void ProveViewContains (params IVisual[] visuals) {
+            ProveContains (this.View.Sink, visuals);
+        }
+
+        public void ProveViewNotContains (params IVisual[] visuals) {
+            ProveNotContains (this.View.Sink, visuals);
+        }
+
+        public SceneFacadeTestWrapper (SceneFacadeTest<TFactory> test) {
+
+            this.Mock = test.Mock;
+            this.Test = test;
+
+            Reset ();
+        }
+
+        public void Reset() {
+            this.Graph =
+                Mock.Scene.Graph.RootSource ().Source
+                as IGraphPair<IVisual, IGraphEntity, IVisualEdge, IGraphEdge>;
+
+            this.View =
+                Mock.Scene.Graph
+                as IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge>;
+
+            this.Nodes = Mock.Factory.Nodes;
+            this.Edges = Mock.Factory.Edges;
+
+            this.Scene = Mock.Scene;
+
+            this.SceneFacade = Mock.SceneFacade;
+            this.Display = Mock.Display;
+        }
+
+        public IGraphScene<IVisual, IVisualEdge> Scene { get; protected set; }
+
+        public GraphSceneFacade<IVisual, IVisualEdge> SceneFacade { get; protected set; }
+
+        public IGraphSceneDisplay<IVisual, IVisualEdge> Display { get; protected set; }
+
+        public IList<IVisual> Nodes { get; protected set; }
+
+        public IList<IVisualEdge> Edges { get; protected set; }
+
+        /// <summary>
+        /// Scene.Graph as <see cref="IGraphPair{IVisual, IVisual, IVisualEdge, IVisualEdge}"/>
+        /// </summary>
+        public IGraphPair<IVisual, IVisual, IVisualEdge, IVisualEdge> View { get; protected set; }
+
+        /// <summary>
+        /// Scene.Graph.RootSource().Source as <see cref="IGraphPair{IVisual, IGraphEntity, IVisualEdge, IGraphEdge}"/>
+        /// </summary>
+        public IGraphPair<IVisual, IGraphEntity, IVisualEdge, IGraphEdge> Graph { get; protected set; }
+
+        public SceneFacadeTest<TFactory> Test { get; protected set; }
+
+        public TestSceneMock<TFactory> Mock { get; protected set; }
+
+        public T TestAs<T> () where T : SceneFacadeTest<TFactory> {
+            return this.Test as T;
+        }
+
+        
+    }
+}
---
+++ src/Limaki.Tests/Limaki.Tests/View/Visuals/GraphSceneMeshTest.cs
@@ -0,0 +1,217 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Limaki.Common;
+using Limaki.Common.Linqish;
+using Limaki.Graphs;
+using Limaki.Graphs.Extensions;
+using Limaki.Model;
+using Limaki.Tests.Graph.Model;
+using Limaki.Tests.Graph.Wrappers;
+using Limaki.Tests.Visuals;
+using Limaki.View.Mesh;
+using Limaki.View.Visuals.UI;
+using Limaki.View.Visuals.Visualizers;
+using Limaki.Visuals;
+using NUnit.Framework;
+using Xwt;
+
+namespace Limaki.Tests.View.Visuals {
+
+    public class GraphSceneMeshTest : DomainTest {
+
+        IGraphSceneMesh<IVisual, IVisualEdge> _mesh = null;
+        protected IGraphSceneMesh<IVisual, IVisualEdge> Mesh { get { return _mesh ?? (Registry.Pool.TryGetCreate<IGraphSceneMesh<IVisual, IVisualEdge>> ()); } }
+
+        public IEnumerable<SceneFacadeTestWrapper<TFactory>> MeshTests<TFactory> (
+            params SceneFacadeTestWrapper<TFactory>[] sources)
+            where TFactory : TestGraphFactory<IGraphEntity, IGraphEdge>, new () {
+
+            var source = sources[0];
+            Mesh.AddDisplay (source.Display);
+
+            foreach (var sink in sources.Skip (1)) {
+                sink.Mock.Scene = Mesh.CreateSinkScene (source.Scene.Graph);
+
+                Mesh.AddDisplay (sink.Display);
+
+                ((TestGraphPairFactory<IVisual, IGraphEntity, IVisualEdge, IGraphEdge>)
+                 sink.Mock.Factory).GraphPair =
+                    // no, its not sink.Graph, and not sink.Scene.... but:
+                    sink.Mock.Scene.Graph.Source<IVisual, IVisualEdge, IGraphEntity, IGraphEdge> ();
+                ;
+
+                // take the inner factorys:
+                var sourceFactory = ((TestGraphPairFactory<IVisual, IGraphEntity, IVisualEdge, IGraphEdge>)
+                                     source.Mock.Factory).Factory;
+
+                var sinkFactory = ((TestGraphPairFactory<IVisual, IGraphEntity, IVisualEdge, IGraphEdge>)
+                                   sink.Mock.Factory).Factory;
+
+                // copy Nodes and Edges
+                var i = 0;
+                foreach (var n in sourceFactory.Nodes) sinkFactory.Nodes[i++] = n;
+                i = 0;
+                foreach (var n in sourceFactory.Edges) sinkFactory.Edges[i++] = n;
+
+                sink.Reset ();
+            }
+
+            return sources;
+        }
+
+        [Test]
+        public void EdgeAddAndChange () {
+
+            var sources = MeshTests<ProgrammingLanguageFactory> (
+                new ProgrammingGraphSceneTest ().Wraps (7));
+
+            var source = sources.First ();
+
+            var iNet = 4; // .Net
+            var iProgramming = 1; // Programming
+            var iJava = 3; // Java 
+
+            var iSink = 0;
+
+            // expand Programming
+            source.Expand (source.Nodes[iProgramming], true);
+
+            foreach (var sink in sources.Skip (1)) {
+                iSink++;
+
+                sink.SetFocused (sink.Nodes[iNet]); 
+                sink.SceneFacade.CollapseToFocused ();
+
+                // add and expand Programming
+                sink.Expand (sink.Nodes[iProgramming], false);
+
+                // test if .Net and Programming in sinkView
+                sink.ProveViewContains (sink.Nodes[iNet], sink.Nodes[iProgramming]);
+
+                // make a new link, add it to source
+            
+                var sourceEdge = source.AddEdge (source.Nodes[iNet], source.Nodes[iProgramming]);
+                source.ProveViewContains (sourceEdge);
+                var backEdge = source.Graph.Get (sourceEdge) as IGraphEdge;
+
+                // testing the sinkGraph
+                Assert.IsTrue (sink.Graph.Source.Contains (backEdge));
+
+                // adding to view
+                source.SceneFacade.Add (sourceEdge, new Point (10, 10));
+                source.Display.Perform ();
+
+                // testing the sinkView
+                var sinkEdge = sink.Graph.Get (backEdge) as IVisualEdge;
+                Assert.IsNotNull (sinkEdge);
+
+                sink.ProveViewContains (sinkEdge);
+
+                // change the link in sink
+
+                // not toggle with each test as the edge is deleted
+                var iNewRoot = iJava; // i % 2 == 1 ? iJava : iNet;
+                var iOldRoot = iNet; // i % 2 == 1 ? iNet : iJava;
+
+                var sinkOldRoot = sink.ChangeLink (sinkEdge, sink.Nodes[iNewRoot], true);
+
+                // test if changed 
+                sink.ProoveChangedLink (sinkEdge, sink.Nodes[iNewRoot], sinkOldRoot, true);
+                source.ProoveChangedLink (sourceEdge, source.Nodes[iNewRoot], source.Nodes[iOldRoot], true);
+
+                sources.ForEach (t => {
+                    var tEdge = t.Graph.Get (backEdge) as IVisualEdge;
+                    t.ProoveChangedLink (tEdge, t.Nodes[iNewRoot], t.Nodes[iOldRoot], true,
+                        // there could be a display without visible root and leaf
+                        t.Scene.Contains (tEdge.Root) && t.Scene.Contains (tEdge.Leaf)
+                        );
+                });
+
+                // remove in sink
+                sink.RemoveEdge (sinkEdge);
+
+                sink.ProveViewNotContains (sinkEdge);
+                sink.ProveNotContains (sink.Graph, sinkEdge);
+                source.ProveViewNotContains (sourceEdge);
+
+                Assert.IsFalse (sink.Graph.Source.Contains (backEdge));
+
+                sources.ForEach (t => {
+                    var tEdge = t.Graph.Get (backEdge) as IVisualEdge;
+                    Assert.IsNull (tEdge);
+                    Assert.IsFalse (t.Graph.Any (e => e.Data == sourceEdge.Data));
+                });
+
+                sink.Expand (sink.Nodes[iProgramming], true);
+                sink.ProveViewNotContains (sinkEdge);
+                sink.SetFocused (sink.Nodes[iNet]);
+                sink.SceneFacade.CollapseToFocused ();
+            }
+        }
+
+        [Test]
+        public void RegisterMesh () {
+
+            var sources = MeshTests<ProgrammingLanguageFactory> (new ProgrammingGraphSceneTest ().Wraps (5));
+            var source = sources.First ();
+            foreach (var sink in sources.Skip (1)) {
+                Assert.AreSame (source.Scene, source.Display.Data);
+                Assert.AreSame (sink.Scene, sink.Display.Data);
+                Assert.AreSame (source.Graph.Source, sink.Graph.Source);
+
+                Assert.IsTrue (Mesh.Scenes.Contains (source.Scene));
+                Assert.IsTrue (Mesh.Scenes.Contains (sink.Scene));
+
+                Assert.AreEqual (source.Nodes.Count, sink.Nodes.Count);
+                Assert.AreEqual (source.Edges.Count, sink.Edges.Count);
+                source = sink;
+            }
+        }
+
+
+        [Test]
+        public void EdgeAddChangeRemove () {
+            var iNet = 4; // .Net
+            var iProgramming = 1; // Programming
+            var iLanguage = 2; // Language
+            var iLibraries = 5; // Libraries
+            var iJava = 3; // Java 
+
+            var Programming2Language = 101;  //[Programming->Language]
+            var Programming2Libraries = 104; //[Programming->Libraries]
+
+            var source = new ProgrammingGraphSceneTest ().Wrap ();
+            EdgeAddChangeRemove (source, iProgramming, iNet, iJava);
+            EdgeAddChangeRemove (source, iProgramming, iNet, iJava);
+            EdgeAddChangeRemove (source, iLibraries, iLanguage, iJava);
+           
+        }
+
+        /// <summary>
+        /// this tests if a link is added, changed and removed 
+        /// </summary>
+
+        public void EdgeAddChangeRemove<TFactory> (SceneFacadeTestWrapper<TFactory> source, int iOne, int iTwo, int iThree)
+            where TFactory : TestGraphFactory<IGraphEntity, IGraphEdge>, new () {
+
+            source.EnsureShape (source.Nodes[iThree]);
+
+            source.SetFocused (source.Nodes[iOne]);
+            source.SceneFacade.CollapseToFocused ();
+
+            source.Expand (source.Nodes[iTwo], false);
+
+            var sourceEdge = source.AddEdge (source.Nodes[iOne], source.Nodes[iTwo]);
+
+            source.ChangeLink (sourceEdge, source.Nodes[iThree], true);
+
+            source.RemoveEdge (sourceEdge);
+
+            source.Expand (source.Nodes[iTwo], true);
+
+            source.ProveViewNotContains (sourceEdge);
+
+        }
+    }
+}
