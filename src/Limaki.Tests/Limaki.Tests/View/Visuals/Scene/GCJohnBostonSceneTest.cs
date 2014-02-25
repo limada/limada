cc16d83809b5d5f177360e3ac6084284820a5a03
lytico <lytico@users.sourceforge.net>
2014-02-25 01:52:54 +0100

    GCJohnBostonSceneTest: refactored to have EdgeExpand-Tests out of Person-Test | PersonJohnGoEdge uses  ReportScene

--- src/Limaki.Tests/Limaki.Tests/View/Visuals/Scene/GCJohnBostonSceneTest.cs
+++ src/Limaki.Tests/Limaki.Tests/View/Visuals/Scene/GCJohnBostonSceneTest.cs
@@ -4,6 +4,7 @@ using Limaki.Tests.Graph.Model;
 using Limaki.Visuals;
 using NUnit.Framework;
 using Limaki.Graphs;
+using System.Linq;
 
 namespace Limaki.Tests.View.Visuals {
 
@@ -36,38 +37,48 @@ namespace Limaki.Tests.View.Visuals {
 
         public override IEnumerable<IVisual> FullExpanded {
             get {
-                return new IVisual[] {
-                    Mock.SampleFactory.Nodes[1], // Person
-                    Mock.SampleFactory.Nodes[2], // John
-                    Mock.SampleFactory.Nodes[3], // City
-                    Mock.SampleFactory.Nodes[4], // Boston
-                    Mock.SampleFactory.Nodes[5], // Go
-                    Mock.SampleFactory.Nodes[6], // Bus
-                    Mock.SampleFactory.Edges[1], // [Person->John]
-                    Mock.SampleFactory.Edges[2], // [City->Boston]
-                    Mock.SampleFactory.Edges[3], // [[Person->John]->Go]
-                    Mock.SampleFactory.Edges[4], // [[[Person->John]->Go]->[City->Boston]]
-                    Mock.SampleFactory.Edges[5], // [[[[Person->John]->Go]->[City->Boston]]->Bus]
-                };
+                yield return Mock.SampleFactory.Nodes[1]; // Person
+                yield return Mock.SampleFactory.Nodes[2]; // John
+                yield return Mock.SampleFactory.Nodes[3]; // City
+                yield return Mock.SampleFactory.Nodes[4]; // Boston
+                yield return Mock.SampleFactory.Nodes[5]; // Go
+                yield return Mock.SampleFactory.Nodes[6]; // Bus
+                yield return Mock.SampleFactory.Edges[1]; // [Person->John]
+                yield return Mock.SampleFactory.Edges[2]; // [City->Boston]
+                yield return Mock.SampleFactory.Edges[3]; // [[Person->John]->Go]
+                yield return Mock.SampleFactory.Edges[4]; // [[[Person->John]->Go]->[City->Boston]]
+                yield return Mock.SampleFactory.Edges[5]; // [[[[Person->John]->Go]->[City->Boston]]->Bus]
+            }
+        }
+
+        public virtual IEnumerable<IVisual> PersonExpanded {
+            get {
+
+                yield return Mock.SampleFactory.Nodes[1]; // Person
+                yield return Mock.SampleFactory.Nodes[2]; // John
+                yield return Mock.SampleFactory.Nodes[5]; // Go
+                yield return Mock.SampleFactory.Edges[1]; // [Person->John]
+                yield return Mock.SampleFactory.Edges[3]; //[[Person->John]->Go]        
+
             }
         }
 
         [Test]
-        public override void FullExpandTest () {
-            base.FullExpandTest ();
+        public override void FullExpandTest() {
+            base.FullExpandTest();
         }
 
         [Test]
-        public void FullExandAndPerson () {
-            this.FullExpandTest ();
-            this.Person ();
+        public void FullExandAndPerson() {
+            this.FullExpandTest();
+            this.Person();
         }
 
         [Test]
-        public void Person () {
-            Mock.Scene.Selected.Clear ();
+        public void Person() {
+            Mock.Scene.Selected.Clear();
             Mock.SetFocused (Mock.SampleFactory.Nodes[1]); // Person
-            Mock.SceneFacade.CollapseToFocused ();
+            Mock.SceneFacade.CollapseToFocused();
 
             Assert.AreEqual (Mock.Scene.Graph.Count, 1);
             Mock.AreEquivalent (new IVisual[] { Mock.SampleFactory.Nodes[1] }, Mock.Scene.Graph);
@@ -75,18 +86,10 @@ namespace Limaki.Tests.View.Visuals {
 
             Mock.SceneFacade.Expand (false);
 
-            var personExpanded = new IVisual[] {
-                Mock.SampleFactory.Nodes[1], // Person
-                Mock.SampleFactory.Nodes[2], // John
-                Mock.SampleFactory.Nodes[5], // Go
-                Mock.SampleFactory.Edges[1], // [Person->John]
-                Mock.SampleFactory.Edges[3], // [[Person->John]->Go]
-            };
-
-            Mock.AreEquivalent (personExpanded, Mock.Scene.Graph);
+            Mock.AreEquivalent (PersonExpanded, Mock.Scene.Graph);
             Mock.ProveShapes (Mock.Scene);
 
-            Mock.Scene.Selected.Clear ();
+            Mock.Scene.Selected.Clear();
             Mock.SetFocused (Mock.SampleFactory.Nodes[5]); // Go
             Mock.SceneFacade.Expand (false);
 
@@ -94,19 +97,64 @@ namespace Limaki.Tests.View.Visuals {
             Mock.ProveShapes (Mock.Scene);
 
             Mock.SceneFacade.RemoveOrphans = true;
-            Mock.SceneFacade.Collapse ();
-            Mock.AreEquivalent (personExpanded, Mock.Scene.Graph);
+            Mock.SceneFacade.Collapse();
+            Mock.AreEquivalent (PersonExpanded, Mock.Scene.Graph);
             Mock.ProveShapes (Mock.Scene);
 
-            Mock.Scene.Selected.Clear ();
+            this.ReportSummary();
+
+        }
+
+        bool edgeWithPerson = false;
+
+        [Test]
+        public void PersonJohnEdge() {
+            if (edgeWithPerson) {
+                Person ();
+                Mock.Scene.Selected.Clear ();
+            } else {
+                Mock.Scene.ClearView ();
+                Mock.SetFocused (Mock.SampleFactory.Nodes[1]); // Person
+
+                Mock.SceneFacade.Expand (false);
+                Mock.CommandsPerform ();
+            }
+
             Mock.SetFocused (Mock.SampleFactory.Edges[1]); // [Person->John]
             Mock.SceneFacade.Expand (false);
+            Mock.CommandsPerform ();
 
+            Mock.ProveLocationNotZero (FullExpanded.ToArray ());
             Mock.AreEquivalent (FullExpanded, Mock.Scene.Graph);
             Mock.ProveShapes (Mock.Scene);
 
-            this.ReportSummary ();
+        }
+
+        [Test]
+        public void PersonJohnGoEdge() {
+
+            if (edgeWithPerson) {
+                Person ();
+                Mock.Scene.Selected.Clear ();
+            } else {
+                Mock.Scene.ClearView ();
+                Mock.SetFocused (Mock.SampleFactory.Nodes[1]); // Person
+
+                Mock.SceneFacade.Expand (false);
+                Mock.CommandsPerform ();
+            }
+
+            Mock.Scene.Selected.Clear();
+            Mock.SetFocused (Mock.SampleFactory.Edges[3]); // [[Person->John]->Go]
+            Mock.SceneFacade.Expand (false);
+            Mock.CommandsPerform ();
+
+            Mock.ReportScene(Mock.Scene);
+            Mock.ProveLocationNotZero (FullExpanded.ToArray());
+            Mock.AreEquivalent (FullExpanded, Mock.Scene.Graph);
+            Mock.ProveShapes (Mock.Scene);
+
 
         }
-    }
+        }
 }
