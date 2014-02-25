9f4a212a910c537dfc7ad63d990ce30683905793
lytico <lytico@users.sourceforge.net>
2014-02-25 18:35:07 +0100

    View.Swf: Painters are obsolete, use Defaut-Painters instead

---
+++ src/Limaki.Tests.View.Swf/[Obosolete]/Painters/VectorPainter.cs
@@ -0,0 +1,43 @@
+/*
+ * Limaki 
+ * 
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ * 
+ * Author: Lytico
+ * Copyright (C) 2006-2011 Lytico
+ *
+ * http://www.limada.org
+ * 
+ */
+
+using System.Drawing;
+using Limaki.Drawing.Painters;
+using Limaki.Drawing.Shapes;
+using System;
+using Xwt.Gdi.Backend;
+
+namespace Limaki.Drawing.Gdi.Painters {
+    [Obsolete]
+    public class VectorPainter : GdiPainter<Vector>, IPainter<IVectorShape, Vector> {
+
+       
+        public override void Render (ISurface surface) {
+            RenderGdi (surface);
+        }
+
+        public override void RenderGdi (ISurface surface) {
+            if ((RenderType.Draw & RenderType) != 0) {
+                Graphics g = ((GdiSurface) surface).Graphics;
+                Vector v = Shape.Data;
+                System.Drawing.Pen pen = ((GdiPen) Style.Pen).Backend;
+                g.DrawLine (pen, v.Start.ToGdi(), v.End.ToGdi());
+            }
+        }
+
+        public override void RenderXwt (ISurface surface) {
+            throw new NotImplementedException ();
+        }
+    }
+}
