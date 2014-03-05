8aa9c42bd7bae2f0e12c45132319ab463dd39338
lytico <lytico@users.sourceforge.net>
2014-03-05 18:33:27 +0100

    Xwt.Application: Initialize with Toolkit as paramter

--- 3rdParty/src/Mono.Xwt/Xwt/Xwt/Application.cs
+++ 3rdParty/src/Mono.Xwt/Xwt/Xwt/Application.cs
@@ -54,7 +54,7 @@ namespace Xwt
 		{
 			if (engine != null)
 				return;
-			Initialize (null);
+			Initialize (default(string));
 		}
 		
 		public static void Initialize (ToolkitType type)
@@ -62,15 +62,21 @@ namespace Xwt
 			Initialize (Toolkit.GetBackendType (type));
 		}
 
-		public static void Initialize (string backendType)
-		{
-			if (backendType == null)
-				throw new ArgumentNullException ("backendType");
-			if (engine != null)
-				return;
+        public static void Initialize (string backendType) 
+        {
+            if (backendType == null)
+                throw new ArgumentNullException ("backendType");
+            if (engine != null)
+                return;
 
-			toolkit = Toolkit.Load (backendType, false);
-			toolkit.SetActive ();
+            toolkit = Toolkit.Load (backendType, false);
+            toolkit.SetActive ();
+            Initialize(toolkit);
+        }
+
+        public static void Initialize (Toolkit toolkit)
+		{
+
 			engine = toolkit.Backend;
 			mainLoop = new UILoop (toolkit);
 
