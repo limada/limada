645296149d79efde50115f9cf64e171a8b0e8c1f
lytico <lytico@users.sourceforge.net>
2014-03-10 20:02:05 +0100

    refactored DependencyVisitor to have a common Source/Sink-Dependency-Method

--- src/Limaki.Core/Graphs/GraphDepencencies.cs
+++ src/Limaki.Core/Graphs/GraphDepencencies.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Limaki.Common;
 using Limaki.Graphs;
+using Limaki.Graphs.Extensions;
 
 namespace Limaki.Graphs {
 
@@ -18,5 +20,34 @@ namespace Limaki.Graphs {
             if (Visitor != null)
                 Visitor(graphCursor, visit, eventType);
         }
+    }
+
+    public class GraphDepencencyExtension {
+
+        public static void DependencyVisitor<TSinkItem, TSinkEdge, TSourceItem, TSourceEdge> (GraphCursor<TSinkItem, TSinkEdge> sink, Action<TSinkItem> visit, GraphEventType eventType)
+            where TSinkEdge : IEdge<TSinkItem>, TSinkItem
+            where TSourceEdge : IEdge<TSourceItem>, TSourceItem {
+
+
+            var graphPair = sink.Graph.Source<TSinkItem, TSinkEdge, TSourceItem, TSourceEdge> ();
+            if (graphPair == null)
+                return;
+
+            var sourceGraph = graphPair.Source;
+            var sourceItem = sink.Graph.SourceItemOf<TSinkItem, TSinkEdge, TSourceItem, TSourceEdge> (sink.Cursor);
+            if (sourceGraph == null || sourceItem == null)
+                return;
+
+            Registry.Pool.TryGetCreate<GraphDepencencies<TSourceItem, TSourceEdge>> ()
+                .VisitItems (
+                    GraphCursor.Create (sourceGraph, sourceItem),
+                    source => {
+                        if (graphPair.Source2Sink.ContainsKey (source))
+                            visit (graphPair.Get (source));
+                    },
+                    eventType);
+
+        }
+
     }
 }
