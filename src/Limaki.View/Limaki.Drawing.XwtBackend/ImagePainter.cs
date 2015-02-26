00d5cd86685753415102c1a978c78ea56a91b118
lytico <lytico@users.sourceforge.net>
2015-02-26 13:21:16 +0100

    Limaki.Drawing.XwtBackend.ImagePainter: enable drawing of VectorImage and PaintingImage

--- src/Limaki.View/Limaki.Drawing.XwtBackend/ImagePainter.cs
+++ src/Limaki.View/Limaki.Drawing.XwtBackend/ImagePainter.cs
@@ -13,12 +13,13 @@
  * 
  */
 
+using System;
 using Limaki.Drawing;
 using Limaki.Drawing.Painters;
 using Limaki.Drawing.Shapes;
-using System;
 using Xwt.Drawing;
 using Xwt;
+using Xwt.Backends;
 
 namespace Limaki.Drawing.XwtBackend {
 
@@ -29,7 +30,7 @@ namespace Limaki.Drawing.XwtBackend {
         public override void Render (ISurface surface) {
             if (Data == null)
                 return;
-            
+
             var contextSurface = (ContextSurface) surface;
             var matrix = contextSurface.Matrix;
             if (matrix.M11 < 0.2d || matrix.M22 < 0.2d)
@@ -37,12 +38,31 @@ namespace Limaki.Drawing.XwtBackend {
             var ctx = contextSurface.Context;
 
             var rect = OuterShape.BoundsRect.Padding (Style.Padding);
+            if (rect.Width < 0.2d || rect.Height < 0.2d)
+                return;
+
             var data = Data;
-            var vecImg = Data as VectorImage;
-            if (vecImg != null) {
-                // TODO: draw on context direct
-                data = vecImg.ToBitmap ();
+            var size = data.Size;
+
+            var ratio = Math.Min (rect.Width / size.Width, rect.Height / size.Height);
+            var loc = new Point (
+                rect.Location.X + (rect.Width - size.Width * ratio) / 2,
+                rect.Location.Y + (rect.Height - size.Height * ratio) / 2);
+
+            var vectorImage = Data as VectorImage;
+            if (vectorImage != null) {
+                rect.Location = loc;
+                DrawImage (vectorImage, ctx, rect, ratio);
+                return;
             }
+
+            var paintingImage = Data as PaintingImage;
+            if (paintingImage != null) {
+                rect.Location = loc;
+                DrawImage (paintingImage, ctx, rect, ratio);
+                return;
+            }
+
             if (rect.Width == data.Width && rect.Height == data.Height)
                 ctx.DrawImage (data, rect.Location);
             else
@@ -50,17 +70,40 @@ namespace Limaki.Drawing.XwtBackend {
         }
 
         public Image Data { get; set; }
-        object IDataPainter.Data { get { return this.Data; } set { this.Data = (Image) value; } }
 
+        object IDataPainter.Data { get { return this.Data; } set { this.Data = (Image) value; } }
 
         public override Point[] Measure (Matrix matrix, int delta, bool extend) {
             if (Data == null)
                 return null;
             var location = Point.Zero;
+            if (OuterShape != null)
+                location = OuterShape.Location;
             var hull = RectangleShape.Hull (new Rectangle (location, Data.Size), delta, extend);
-            if (matrix != null)
+            if (matrix != null && !matrix.IsIdentity)
                 matrix.Transform (hull);
             return hull;
+        }
+        
+        protected virtual void DrawImage (VectorImage image, Context ctx, Rectangle bounds, double ratio) {
+            ctx.Save ();
+            ctx.Translate (bounds.Location);
+            var size = image.Size;
+
+            var data = image.Data;
+            var engine = ((IFrontend) image).ToolkitEngine;
+            engine.VectorImageRecorderContextHandler.Draw (ctx.Handler, Toolkit.GetBackend (ctx), data);
+            ctx.Restore ();
+        }
+
+        protected virtual void DrawImage (PaintingImage image, Context ctx, Rectangle bounds, double ratio) {
+            ctx.Save ();
+            ctx.Translate (bounds.Location);
+            var size = image.Size;
+            ctx.Scale (ratio, ratio);
+            image.Paint (ctx);
+
+            ctx.Restore ();
         }
     }
 }
