86b643a1ed5e2536d9e84c1af300075c10958453
lytico <lytico@users.sourceforge.net>
2015-04-24 00:18:26 +0200

    introduce some playground ideas on Richtext and Markdown

---
+++ src/Limaki.Tests/Playground/Text/MarkDown.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Limaki.Playground.Text {
+
+    public interface IMarkDownEditor {
+
+    }
+
+    public interface IMarkDownViewer {
+
+    }
+
+    public class MarkDownEdit {
+        public IMarkDownEditor Editor { get; set; }
+        public IMarkDownViewer Viewer { get; set; }
+
+        public void StartEdit () {
+            // hide the viewer and show the editor
+        }
+        public void EndEdit () {
+            // show the viewer and hide the editor
+        }
+
+    }
+}
---
+++ src/Limaki.Tests/Playground/Text/RichText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Limaki.Playground.Text {
+    /// <summary>
+    /// like Winforms, Wpf
+    /// </summary>
+    public class RichTextBox {
+    }
+
+    /// <summary>
+    /// mix of Gkt (TextView) and Win
+    /// </summary>
+    public class RichTextView {
+    }
+}
