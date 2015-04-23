f34986f72377456bad24385749322a52af01e670
lytico <lytico@users.sourceforge.net>
2015-04-24 00:21:37 +0200

    something with Markdown

--- src/Limaki.Tests/Limaki.Tests/Common/Rtf/RtfImporterTest.cs
+++ src/Limaki.Tests/Limaki.Tests/Common/Rtf/RtfImporterTest.cs
@@ -7,8 +7,16 @@ namespace Limaki.Tests.Common.Rtf {
     [TestFixture]
     public class RtfImporterTest : DomainTest {
         [Test]
-        public void TestParse () {
+        public void TestParseHtml () {
             var doc = new HtmlDocument ();
+            var importer = new RtfImporter (ByteUtils.AsAsciiStream (RtfTestData.WordGameUmlaut), doc);
+            importer.Import ();
+            ReportDetail (doc.Body);
+        }
+
+        [Test]
+        public void TestParseMarkdown () {
+            var doc = new MarkdownDocument ();
             var importer = new RtfImporter (ByteUtils.AsAsciiStream (RtfTestData.WordGameUmlaut), doc);
             importer.Import ();
             ReportDetail (doc.Body);
