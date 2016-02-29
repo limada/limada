5b649f5be090c1e6ae72346fe7785d93d465a679
lytico <lytico@users.sourceforge.net>
2016-02-29 21:47:14 +0100

    add clean.sh for linux

---
+++ src/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#set -x
+
+recursiverm() {
+   printf 'clean '$1'\n'
+   for f in $(find $1 -type d -iname "bin" -or -iname "obj" -or -iname '_resharper*' -or -iname '*.svn*'); do 
+     printf $f'\n'
+     rm -rf $f
+   done
+   find $1 -type f -iname "*.user" -or -iname "*.resharper" -or -iname '*.pidb' -or -iname '*.pdb' -delete
+}
+
+
+recursiverm "."
+recursiverm "../3rdParty/src/"
+
+set +x
+printf 'done\n'
+sleep 5
+
