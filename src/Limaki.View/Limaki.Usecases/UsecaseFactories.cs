c84c7d6ca3492ef1f0e9faa81392bfcfb238e9b6
lytico <lytico@users.sourceforge.net>
2016-02-29 21:47:40 +0100

    cleanup

---
+++ src/Limaki.View/Limaki.Usecases/UsecaseFactories.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Limaki.Usecases {
+
+    public class UsecaseFactories<T> : IEnumerable<UsecaseFactory<T>> where T:new() {
+
+        private ICollection<UsecaseFactory<T>> factories = new List<UsecaseFactory<T>>();
+
+        public void Add(UsecaseFactory<T> f) {
+            factories.Add(f); 
+        }
+
+        public IEnumerator<UsecaseFactory<T>> GetEnumerator() {
+            return factories.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ src/Limaki.View/Limaki.Usecases/UsecaseFactory.cs
@@ -0,0 +1,38 @@
+/*
+ * Limaki 
+ * 
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ * 
+ * Author: Lytico
+ * Copyright (C) 2006-2010 Lytico
+ *
+ * http://www.limada.org
+ * 
+ */
+
+using Limaki.Common;
+using Limaki.View;
+
+namespace Limaki.Usecases {
+    public class UsecaseFactory<T> where T:new() {
+
+        public virtual T Create() {
+            var result = new T();
+            return result;
+        }
+
+        public IComposer<T> Composer { get; set; }
+        public IComposer<T> BackendComposer { get; set; }
+
+        public virtual void Compose(T useCase) {
+            Composer.Factor(useCase);
+            BackendComposer.Factor(useCase);
+
+            BackendComposer.Compose(useCase);
+            Composer.Compose(useCase);
+
+        }
+    }
+}
