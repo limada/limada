36da301288e224d14244e4c496f71874770a0e21
lytico <lytico@users.sourceforge.net>
2017-01-26 16:52:54 +0100

    Xwt.Swf.Xwt.SwfBackend: introduce MenuBackend

---
+++ src/Xwt.Swf/Xwt.Swf/Xwt.SwfBackend/MenuBackend.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xwt.Backends;
+using SWF = System.Windows.Forms;
+using SD = System.Drawing;
+
+namespace Xwt.Swf.Xwt.SwfBackend {
+
+    public class MenuBackend : Backend<Menu>, IMenuBackend {
+        public SWF.MenuStrip Menu { get; protected set; }
+
+        internal SWF.ToolStripItemCollection ItemsCollection { get; set; }
+
+        ObservableCollection<IMenuItemBackend> _items = new ObservableCollection<IMenuItemBackend> (); 
+        public override void InitializeBackend (object frontend, ApplicationContext context) {
+            base.InitializeBackend (frontend, context);
+            Menu = new SWF.MenuStrip ();
+            ItemsCollection = Menu.Items;
+            _items.CollectionChanged += (sender,args) => {
+                var backend = args.NewItems.Cast<MenuItemBackend> ().FirstOrDefault();
+                if (args.Action == NotifyCollectionChangedAction.Add) {
+                    ItemsCollection.Insert (args.NewStartingIndex, backend.MenuItem);
+                }
+                if (args.Action == NotifyCollectionChangedAction.Remove) {
+                    ItemsCollection.Remove (backend.MenuItem);
+                }
+            };
+        }
+
+        public object Font {
+            get { return Menu.Font; }
+            set { Menu.Font = (SD.Font)value; }
+        }
+
+
+        public override void DisableEvent (object eventId) {
+        }
+
+        public override void EnableEvent (object eventId) {
+        }
+
+        public void InsertItem (int index, IMenuItemBackend menuItem) {
+           _items.Add (menuItem);
+            
+        }
+
+        public void RemoveItem (IMenuItemBackend menuItem) {
+            _items.Remove (menuItem);
+        }
+
+        public void Popup () {
+        }
+
+        public void Popup (IWidgetBackend widget, double x, double y) {
+        }
+
+
+    }
+}
+
---
+++ src/Xwt.Swf/Xwt.Swf/Xwt.SwfBackend/MenuItemBackend.cs
@@ -0,0 +1,86 @@
+using System;
+using Xwt.Backends;
+
+namespace Xwt.Swf.Xwt.SwfBackend {
+    public class MenuItemBackend : Backend<MenuItem>, IMenuItemBackend {
+        
+        public System.Windows.Forms.ToolStripItem MenuItem { get; protected set; }
+
+        public override void InitializeBackend (object frontend, ApplicationContext context) {
+            base.InitializeBackend (frontend, context);
+            MenuItem = new System.Windows.Forms.ToolStripMenuItem ();
+        }
+
+        private IMenuItemEventSink eventSink;
+        public void Initialize (IMenuItemEventSink eventSink) {
+            this.eventSink = eventSink;
+        }
+
+        public override void EnableEvent (object eventId) {
+            if (MenuItem == null)
+                return;
+
+            if (eventId is MenuItemEvent) {
+                switch ((MenuItemEvent)eventId) {
+                    case MenuItemEvent.Clicked:
+                        this.MenuItem.Click += MenuItemClickHandler;
+                        break;
+                }
+            }
+        }
+
+        public override void DisableEvent (object eventId) {
+            if (MenuItem == null)
+                return;
+
+            if (eventId is MenuItemEvent) {
+                switch ((MenuItemEvent)eventId) {
+                    case MenuItemEvent.Clicked:
+                        this.MenuItem.Click -= MenuItemClickHandler;
+                        break;
+                }
+            }
+        }
+
+        void MenuItemClickHandler (object sender, EventArgs args) {
+            Context.InvokeUserCode (eventSink.OnClicked);
+        }
+
+        public string Label {
+            get { return MenuItem.Text; }
+            set { MenuItem.Text = value; }
+        }
+
+        public bool Sensitive {
+            get { return MenuItem.Available; }
+            set { MenuItem.Available = value; }
+        }
+
+        public bool UseMnemonic {
+            get { return false; }
+            set { }
+        }
+
+        public bool Visible {
+            get { return MenuItem.Visible; }
+            set { MenuItem.Visible = value; }
+        }
+
+
+        public void SetImage (ImageDescription image) {
+        }
+
+        public void SetSubmenu (IMenuBackend menu) {
+            var backend = (MenuBackend)menu;
+            var tsmi = (this.MenuItem as System.Windows.Forms.ToolStripMenuItem);
+            foreach (System.Windows.Forms.ToolStripItem item in backend.Menu.Items) {
+                item.Owner = null;
+                tsmi.DropDownItems.Add (item);
+            }
+            backend.ItemsCollection = tsmi.DropDownItems;
+            //tsmi.DropDownItems.AddRange (backend.Menu.Items);
+
+        }
+
+    }
+}
