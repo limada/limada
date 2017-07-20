51e32b907b1169a05338e742891caa94f9614d6e
lytico <lytico@users.sourceforge.net>
2017-07-20 15:55:19 +0200

    [Xwt] Distance: add operators

---
+++ 3rdParty/src/Mono.Xwt/Xwt/Limaki.Extended/Distance.Extended.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Xwt {
+	
+	public partial struct Distance 
+	{
+		public static Distance operator * (Distance d1, Distance d2) => new Distance (d1.dx * d2.dx, d1.dy * d2.dy);
+
+		public static Distance operator * (Distance d1, double value) => new Distance (d1.dx * value, d1.dy * value);
+
+		public static Distance operator / (Distance d1, double value) => new Distance (d1.dx / value, d1.dy / value);
+
+		public static Distance operator + (Distance d1, double value) => new Distance (d1.dx + value, d1.dy + value);
+
+		public static Distance operator - (Distance d1, double value) => new Distance (d1.dx - value, d1.dy - value);
+
+	}
+}
