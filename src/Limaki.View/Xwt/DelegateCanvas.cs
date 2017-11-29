fb5f560ad828dbb56514efd7adfe6b60e9169a60
lytico <lytico@users.sourceforge.net>
2017-11-29 17:48:55 +0100

    Limaki: add DelegateCanvas

---
+++ src/Limaki.View/Xwt/DelegateCanvas.cs
@@ -0,0 +1,15 @@
+﻿using Xwt.Drawing;
+using System;
+
+namespace Xwt.Widgets {
+    
+    public class DelegateCanvas : Canvas {
+
+        public Action<Context, Rectangle> Draw { get; set; }
+
+        protected override void OnDraw (Context ctx, Rectangle dirtyRect) {
+            base.OnDraw (ctx, dirtyRect);
+            Draw.Invoke (ctx, dirtyRect);
+        }
+    }
+}
