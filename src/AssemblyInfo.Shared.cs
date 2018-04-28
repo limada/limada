09adc4674466cde3e307543d3ee83b0c4ab3b645
lytico <lytico@users.sourceforge.net>
2018-04-28 18:58:39 +0200

    Limaki.AssemblyInfo.Shared: pump version to 18.04

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("www.limada.org")]
-[assembly: AssemblyCopyright ("© Lytico 2006 - 2017 (http://github.com/lytico)")]
+[assembly: AssemblyCopyright ("© Lytico 2006 - 2018 (http://github.com/lytico)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("17.10.0.0")]
-[assembly: AssemblyFileVersion("17.10.0.0")]
+[assembly: AssemblyVersion("18.04.0.0")]
+[assembly: AssemblyFileVersion("18.04.0.0")]
 
 
