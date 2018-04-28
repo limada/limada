bec856f6a520d77089aeaf3abb4465acbb592d81
lytico <lytico@users.sourceforge.net>
2018-04-28 18:51:29 +0200

    Xwt.Tests: adjust AssemblyInfo

--- src/Xwt.Tests/Properties/AssemblyInfo.cs
+++ src/Xwt.Tests/Properties/AssemblyInfo.cs
@@ -6,31 +6,6 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle ("Xwt.Tests")]
+[assembly: AssemblyProduct("Xwt.Tests")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Name")]
-[assembly: AssemblyProduct ("Xwt.Tests")]
-[assembly: AssemblyCopyright ("Copyright © Name 2012")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible (false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid ("5c092b15-75dc-416a-8b98-0c0de12940ee")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
