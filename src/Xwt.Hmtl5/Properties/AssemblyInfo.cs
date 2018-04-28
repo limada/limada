bf4f0fac7735f9774fc456b59430b4fb106bc879
lytico <lytico@users.sourceforge.net>
2018-04-28 18:38:37 +0200

    Xwt.Html5: migrate to .netstandard2

--- src/Xwt.Hmtl5/Properties/AssemblyInfo.cs
+++ src/Xwt.Hmtl5/Properties/AssemblyInfo.cs
@@ -6,31 +6,6 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle ("Xwt.Hmtl5")]
+[assembly: AssemblyProduct("Xwt.Hmtl5")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Name")]
-[assembly: AssemblyProduct ("Xwt.Hmtl5")]
-[assembly: AssemblyCopyright ("Copyright © Name 2012")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible (false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid ("04dab694-9c63-4ce1-8f13-2e8754f7a1cc")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
