0059883b45d2bd4e88c56f2fb2dd396c0ec9b34a
lytico <lytico@users.sourceforge.net>
2018-12-27 14:25:12 +0100

    Limaki.View.Headless: move to own project

--- src/Limaki.View.Headless/Limaki.View.Headless/HeadlessContextResourceLoader.cs
+++ src/Limaki.View.Headless/Limaki.View.Headless/HeadlessContextResourceLoader.cs
@@ -38,7 +38,7 @@ namespace Limaki.View.Headless {
             if (Toolkit.CurrentEngine == null) {
                 tk = Toolkit.CreateToolkit<HeadlessEngine> (false);
                 tk.SetActive();
-                Xwt.Application.Initialize (tk);
+                Xwt.Application.Initialize (tk.Type);
             }
 
             if (tk.Backend is HeadlessEngine) {
