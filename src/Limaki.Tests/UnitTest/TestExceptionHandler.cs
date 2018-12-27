ad25f1d8768bcd1699a50b4afd90e4633b6c5b22
lytico <lytico@users.sourceforge.net>
2018-12-27 14:33:17 +0100

    Limaki.UnitTests: introduce TestExceptionHandler (from Headless)

---
+++ src/Limaki.Tests/UnitTest/TestExceptionHandler.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Limaki 
+ * 
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ * 
+ * Author: Lytico
+ * Copyright (C) 2006-2011 Lytico
+ *
+ * http://www.limada.org
+ * 
+ */
+
+using System;
+using Limaki.Common;
+
+namespace Limaki.UnitTest {
+    
+    public class TestExceptionHandler : IExceptionHandler {
+
+        public void Catch (Exception e) {
+            throw e;
+        }
+
+        public void Catch (Exception e, MessageType messageType) {
+            throw e;
+        }
+    }
+}
