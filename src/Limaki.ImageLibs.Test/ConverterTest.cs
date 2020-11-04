1b2abac1b93fcdc2cde10150da0c29933c14c068
lytico <lytico@users.sourceforge.net>
2020-11-04 23:21:32 +0100

    Limaki.ImageLibs.Test.csproj: convert to Sdk-Style & adjust dependencies

---
+++ src/Limaki.ImageLibs.Test/ConverterTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Limaki.Common;
+using Limaki.Common.IOC;
+using Limaki.Contents;
+using Limaki.Tests;
+
+namespace Limaki.ImageLibs.Test {
+    [TestFixture]
+    public class ConverterTest {
+        [SetUp]
+        public void Register() {
+            if (Registry.ConcreteContext == null)
+                Registry.ConcreteContext = new ApplicationContext();
+            new ImageLibResourceLoader().ApplyResources (Registry.ConcreteContext);
+        }
+
+        [Test]
+        public void FindConverter () {
+            var converterPool = Registry.Pooled<ConverterPool<Stream>>();
+            var conv = converterPool.Find(ContentTypes.TIF,ContentTypes.PNG);
+            Assert.IsTrue (conv is ImageConverter);
+        }
+
+        [Test]
+        public void TestTif2Png() {
+            var conv = new ImageConverter();
+            var tifFile = File.OpenRead(TestLocations.TestDataDir + "sample.tif");
+            var pngFile = File.Create("bw1.png");
+            var result = conv.Tif2Png (tifFile, pngFile);
+            Console.WriteLine($"{result}");
+            Assert.IsNull (result);                              
+            pngFile.Close();
+        }
+    }
+}
---
+++ src/Limaki.ImageLibs.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("ImageLibs.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("ImageLibs.Test")]
